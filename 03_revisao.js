7cef2725438b2b40e19a80b789f72010e7931ef4
guilhermeTI007 <guilhermesenacevolui@gmail.com>
2026-08-17 14:47:34 -0300

    17/08

---
+++ 03_revisao.js
@@ -0,0 +1,24 @@
+let aluguel = 1200.00
+let condominio = 250.50
+let internet = 89.90
+let total = condominio + internet + aluguel
+
+console.log("o total das contas é: " + total)
+
+let nome = "guilherme"
+let sobrenome = "favero"
+let nome_completo = nome +" "+ sobrenome
+console.log(nome_completo)
+
+
+
+let salario = 2000
+let comissao = 100
+console.log(salario)
+
+salario = salario + comissao
+console.log(salario)
+
+let adicional_noturno = 500
+salario = salario + adicional_noturno
+console.log(salario)
