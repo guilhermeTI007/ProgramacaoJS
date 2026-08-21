191118da3819ec54f4006d4e37cff426a4ee9564
guilhermeTI007 <guilhermesenacevolui@gmail.com>
2026-08-21 13:47:46 -0300

    concluido

--- 06 atividades/06_atividade_6.js
+++ 06 atividades/06_atividade_6.js
@@ -7,4 +7,19 @@ let moraVitima = prompt("Mora perto da vítima?")
 let devia = prompt("Devia para a vítima?")
 let trabalhouVitima = prompt("Já trabalhou com a vítima?")
 
+let totalStatus =
+    (telefone == "sim") +
+    (local == "sim") +
+    (moraVitima == "sim")+
+    (devia == "sim")+
+    (trabalhouVitima == "sim")
 
+if ( totalStatus == 5 ){
+    console.log("VOCE É ASSASSINO")
+}else if(totalStatus == 4 || totalStatus == 3){
+    console.log("VOCE É CUMPLICE")
+}else if(totalStatus == 2){
+    console.log("VOCE É SUSPEITO")
+}else{
+    console.log("VOCE É INOCENTE")
+}
