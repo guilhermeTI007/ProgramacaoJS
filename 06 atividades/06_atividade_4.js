8e119d8ee9fb0781d5155a5b649b91531464d377
guilhermeTI007 <guilhermesenacevolui@gmail.com>
2026-08-21 14:17:18 -0300

    feito

--- 06 atividades/06_atividade_4.js
+++ 06 atividades/06_atividade_4.js
@@ -2,10 +2,12 @@ let prompt = require("prompt-sync")()
 
 let pesoPeixe = parseInt(prompt("digite o peso total dos peixes: "))
 let excesso = pesoPeixe - 50
-
+let valorPeixe = 5.5
 if( pesoPeixe <= 50 ){
-    console.log("sem multa")
+    valorPeixe = valorPeixe * pesoPeixe
+    console.log("sem multa,valor total é " + valorPeixe)
 }else{
     excesso = excesso * 4.25
-    console.log(" tera que pagar uma multa de: R$" + excesso)
+    valorPeixe = valorPeixe * pesoPeixe + excesso
+    console.log(" tera que pagar uma multa de: R$" + excesso +" total apagar é R$" + valorPeixe)
 }
